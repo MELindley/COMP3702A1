624a850abc7e1b7759d06b03d953c8b14246b957
Unknown <marco.lindley@hotmail.fr>
2017-08-19 16:42:08 +1000

    removed bin updated the reader class

---
+++ src/src/model/Graph.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/***
+ * Model Class for the environment graph
+ * A Graph contains all of the junctions (Vertices) and roads (Edges) described in the input. 
+ * @author Marco Lindley
+ *
+ */
+public class Graph implements Cloneable {
+	HashSet<Vertex>junctions;
+	HashSet<Edge> roads;
+	
+	
+	
+	public Graph(){
+		junctions = new HashSet<Vertex>();
+		roads= new HashSet<Edge>();
+	}
+	
+	public Graph(HashSet<Vertex> vertices,HashSet<Edge> edges) {
+		this.junctions = vertices;
+		this.roads = edges;
+	}
+	
+	public void addJunction(Vertex loc){
+		if(!junctions.contains(loc))
+			junctions.add(loc);
+	}
+	
+	public void addRoad(Edge e){
+		if(!roads.contains(e)){
+			this.roads.add(e);
+		}
+	}
+	
+	public HashSet<Vertex> getJunctions() {
+		return junctions;
+	}
+	
+	public HashSet<Edge> getRoads() {
+		return roads;
+	}
+
+	public int getNumberOfJunctions() {
+		return this.junctions.size();
+	}
+
+	public void setJunctions(HashSet<Vertex> junctions) {
+		this.junctions = junctions;
+	}
+
+	public void setEdges(HashSet<Edge> edges) {
+		this.roads = edges;
+	}
+
+	
+	public Vertex getVertexById(int id){
+		for(Vertex v: junctions){
+			if(v.getId()==id){
+				return v;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return "Graph [Junctions=" + junctions + ", Roads=" + roads
+				+ "]";
+	}
+	
+}
---
+++ src/src/model/Query.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Query {
+	String initID;
+	String goalID;
+	public Query( String initID, String goalID){
+		this.initID = initID;
+		this.goalID =goalID;
+	}
+
+	public String getInitID() {
+		return initID;
+	}
+
+	public String getGoalID() {
+		return goalID;
+	}
+
+	public void setInitID(String initID) {
+		this.initID = initID;
+	}
+
+	public void setGoalID(String goalID) {
+		this.goalID = goalID;
+	}
+
+	@Override
+	public String toString(){
+		return "Fing optimal path from: "+initID+" to: "+goalID;
+	}
+}
